80274c22ad78e7a7bd44ff15734ebb89f32499a1
Charline <charline.courel@viacesi.fr>
2019-01-08 10:17:29 +0100

    first commit

---
+++ exo_1.php
@@ -0,0 +1,31 @@
+<?php
+	$groupe= [
+			'personne_1 '=> [
+				'nom'=> 'Courel',
+				'prenom'=>'Charline',
+				'age'=> '22'
+			],
+
+			'personne_2' => [
+				'nom'=> 'Grrrr',
+				'prenom'=>'Pierre',
+				'age'=> '13'
+			],
+
+			'personne_3' => [
+				'nom'=> 'Marco',
+				'prenom'=>'Polo',
+				'age'=> '18'
+			]	
+		];
+
+	var_dump($groupe[personne_2 'prenom']);
+
+	//foreach ($groupe as $prenom) {
+		//	echo $prenom."\n";
+		/*if ($age => 18) {
+			echo $prenom.'a plus de 18ans'."\n";
+		}*/
+//	}
+
+?>
